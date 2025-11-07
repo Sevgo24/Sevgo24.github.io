c7f2b8ea9ffc9144717bf54dbd51365abec7fba1
OBlas <blas0123.22@gmail.com>
2025-11-07 01:33:04 -0500

    ci: add deploy to gh-pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { componentTagger } from 'lovable-tagger'
 
 export default defineConfig(({ mode }) => ({
+  base: '/Smart_College/',
   plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   server: {
     host: '::',
